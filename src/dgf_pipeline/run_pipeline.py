1293c178e57fffd2876da5a6ce59d14905f24851
ljc <ljc@pku>
2025-06-11 22:03:22 +0800

    run_pipe

--- src/dgf_pipeline/run_pipeline.py
+++ src/dgf_pipeline/run_pipeline.py
@@ -1,4 +1,3 @@
-
 import argparse
 from dgf_header_parser.extractor import Extractor
 from dgf_prompt_generator.generator import PromptGenerator
@@ -11,6 +10,6 @@ if __name__ == "__main__":
     parser.add_argument('--samples', type=int, default=10)
     args = parser.parse_args()
 
-    # 直接调用模块5
+    # 直接调用
     fc = FeedbackController(api_json=args.api_json, output_dir=args.output_dir)
     fc.run_iteration(num_samples=args.samples)
