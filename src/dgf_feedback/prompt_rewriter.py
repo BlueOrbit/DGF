10a23d447402804093bd778246f16740e2a47abc
ljc <lanjiachen@stu.pku.edu.cn>
2025-06-12 12:08:00 +0800

    删除冗余文件

--- src/dgf_feedback/prompt_rewriter.py
+++
@@ -1,8 +0,0 @@
-class PromptRewriter:
-    def __init__(self, base_prompt_template):
-        self.base_template = base_prompt_template
-
-    def rewrite_prompt(self, prev_success_count):
-        num_funcs = 5 + min(prev_success_count, 5)  # 每轮逐渐增加API组合数量
-        return self.base_template.generate_prompt(num_funcs=num_funcs)
-
